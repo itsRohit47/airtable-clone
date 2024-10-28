5000b49e744b4765f7456f05ebc0937f750d6ac2
Rohit Bajaj <rohitbajaj13@yahoo.com>
2024-10-28 17:29:45 +1100

    latest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { HydrateClient } from "@/trpc/server";
 import { getServerAuthSession } from "@/server/auth";
 import Link from "next/link";
-import { signOut } from "next-auth/react";
 
 export default async function Home() {
   const session = await getServerAuthSession();
@@ -12,12 +11,7 @@ export default async function Home() {
         {session ? (
           <div>
             Logged in as {session.user.email}
-            <button
-              onClick={() => signOut({ callbackUrl: "/" })}
-              className="rounded-md bg-[#3b49df] px-4 py-2 text-base text-white"
-            >
-              <div className=""> Yes, sign out</div>
-            </button>
+            <Link href="/logout">Logout</Link>
           </div>
         ) : (
           <div className="flex flex-col items-center">
