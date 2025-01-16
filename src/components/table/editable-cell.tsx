9607646fbe9b6122908d4f09c2d4b42d7144a41d
Rohit Bajaj <daoanh440@gmail.com>
2025-01-16 19:56:59 +1100

    hgss

--- src/components/table/editable-cell.tsx
+++ src/components/table/editable-cell.tsx
@@ -28,7 +28,7 @@ export function EditableCell({
 
   const { mutate } = api.table.updateCell.useMutation({
     onSettled: () => {
-      void ctx.table.getData.invalidate();
+      // void ctx.table.getData.invalidate();
       setIsInvalid(false);
       setLoading(false);
     },
